dc740b1743b9107cc356c309e557a67e45258100
William <williambassedone@outlook.com>
2025-11-26 16:38:09 -0300

    Ajuste 2

--- routes/web.php
+++ routes/web.php
@@ -5,14 +5,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/clear-cache', function () {
-    try {
-        \Illuminate\Support\Facades\Artisan::call('route:clear');
-        \Illuminate\Support\Facades\Artisan::call('config:clear');
-        \Illuminate\Support\Facades\Artisan::call('cache:clear');
-        return 'Cache Cleared!';
-    } catch (\Exception $e) {
-        return 'Error clearing cache: ' . $e->getMessage();
-    }
-});
